4fc353e5095a54d13697170a8bbfd1ff08d5499b
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-04-05 14:17:37 +0200

    rewrite code. and improvements

--- js/script.js
+++ js/script.js
@@ -1,34 +1,134 @@
 "use strict";
 
-function nomeFunzione() {
-    let scopeFunzione = 'parent';
-    for (let i = 1; i <= 10; i++) {
-        console.log(scopeFunzione);
-        const pippo = 'personaggio Disney';
-    }
-}
-
-
-
-//funzione di callback
-function stampa(nome, cognome) {
-    let nomeCognome = nome + ' ' + cognome;
-    document.writeln(nomeCognome);
-}
-
-//Posso dare dei valori di default dentro le parentesi della funzione
-//siccome i valori della funzione li definisco dopo, metto print per adesso
-function nomeFunzione(nome, cognome, print) {
-    nome = nome.toUpperCase();
-    cognome = cognome.toUpperCase();
-    print(nome, cognome);
-}
-
-//Qui definisco i valori della funzione e siccome il terzo è un valore(in questo caso di una funzione, metto stampa. Che è il valore di un altra funzione.)
-nomeFunzione('', '', stampa);
-///////////////////////////////
-
 function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+const BOMB_NUMBER = 16;
+const bombs = [];
+let MAX_ATTEMPT;
+let ATTEMPTS = 0;
+/////////////////
+document.getElementById('play').addEventListener('click', setLevel);
+
+
+function setLevel(event) {
+    const level = document.getElementById('difficulty').value;
+    let numSquare;
+    switch (level) {
+        case '1':
+        default:
+            numSquare = 100;
+            break;
+        case '2':
+            numSquare = 81;
+            break;
+        case '3':
+            numSquare = 49;
+            break;
+    }
+
+    let cellPerSide = Math.sqrt(numSquare);
+    generateBomb(numSquare);
+    generaGriglia(numSquare, cellPerSide);
+}
+
+function generateBomb(numSquare) {
+    MAX_ATTEMPT = numSquare - BOMB_NUMBER;
+    while (bombs.length < BOMB_NUMBER || bombs.length <= 0) {
+        let bombNumber = getRandomInt(1, numSquare);
+        if (!bombs.includes(bombNumber)) {
+            bombs.push(bombNumber);
+        }
+    }
+    console.log(bombs)
+}
+
+function generaGriglia(numSquare, cellPerSide) {
+    const app = document.getElementById('app');
+    app.innerHTML = '';
+    let row = document.createElement('div');
+    row.setAttribute('class', 'gridrow');
+    for (let i = 1; i <= numSquare; i++) {
+        const square = generaCella(i, cellPerSide);
+        row.append(square)
+    }
+    app.append(row)
+}
+
+function generaCella(numCell, cellPerSide) {
+    let square = document.createElement('div');
+    square.setAttribute('class', 'box');
+    square.style.width = `calc(100% / ${cellPerSide})`;
+    square.style.height = `calc(100% / ${cellPerSide})`;
+    square.classList.add('pointer');
+    square.innerHTML = `<span>${numCell}</span>`
+    square.addEventListener('click', coloraCella);
+    return square;
+}
+
+function coloraCella() {
+    let num = parseInt(this.innerText);
+    ATTEMPTS++;
+    if (bombs.includes(num)) {
+        this.style.backgroundColor = "red";
+        //this.innerHTML = `<img src=""`
+        gameOver();
+    } else {
+        this.style.backgroundColor = "rgb(0 225 101)";
+    }
+
+    console.log(this.innerText)
+
+    this.classList.remove('pointer');
+
+    this.style.color = "rgb(0 0 0)";
+    this.removeEventListener("click", coloraCella)
+}
+
+function gameOver() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////
+
+/*function getRandomInt(min, max) {
 
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -38,8 +138,6 @@ function getRandomInt(min, max) {
 
 
 document.getElementById('play').addEventListener('click', stampareGriglia);
-
-
 
 //Stampare griglia
 function stampareGriglia() {
@@ -66,7 +164,7 @@ function stampareGriglia() {
 }
 
 
-stampareGriglia();
+
 //////////////////
 
 //Funzione click
@@ -117,4 +215,4 @@ function creaColonne() {
         }
         return cols;
     }
-}
+}*/
